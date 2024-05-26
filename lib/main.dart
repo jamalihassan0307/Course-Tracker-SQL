bd5ea210ba88151271f4b8d988eff8c0b30a10db
jamalihassan0307 <jamalihassan0307@gmail.com>
2024-05-26 14:19:17 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/services.dart';
+// import 'package:course_tracker/model.dart/RecppeModel.dart';
+import 'package:course_tracker/sql/sql.dart';
+// import 'package:course_tracker/sql/sqllite.dart';
+import 'package:course_tracker/screen/loginScreen.dart';
+import 'package:course_tracker/widget/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: mainTheme.scaffoldBackgroundColor,
+      statusBarBrightness: Brightness.light));
+  runApp(const MyApp());
+  await SQL.connection();
+// var list= await loadRecipes();
+//   for (var e in list) {
+    
+//     SQL.get("INSERT INTO dbo.Brecipes VALUES (${e.toMap()})");}
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Dairy App',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: mainTheme.scaffoldBackgroundColor),
+        useMaterial3: true,
+      ),
+
+      home: const LoginScreen(),
+      // HomeScreen(),
+      darkTheme: mainTheme,
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
