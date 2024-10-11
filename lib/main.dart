08fc4b4c4e9677bbba2f65c35ebfbaeb0c0caf41
jamalihassan0307 <jamalihassan0307@gmail.com>
2024-10-11 06:48:29 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
-import 'core/locator.dart';
-import 'core/providers.dart';
-import 'core/services/navigator_service.dart';
+import 'package:course_tracker/screen/SignupScreen.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'views/home/home_view.dart';
 
 void main() async {
-  await LocatorInjector.setupLocator();
-  runApp(MainApplication());
+  // await LocatorInjector.setupLocator();
+  runApp(const MainApplication());
 }
 
 class MainApplication extends StatelessWidget {
+  const MainApplication({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: ProviderInjector.providers,
-      child: MaterialApp(
-        navigatorKey: locator<NavigatorService>().navigatorKey,
-        home: HomeView(),
-      ),
+    return const MaterialApp(
+      // navigatorKey: locator<NavigatorService>().navigatorKey,
+      home: SignUp(),
     );
   }
 }
